db85c1973d69c0d92a7d05ef01d447dbf5189c34
Pratikchhya <pratikchhyaa@gmail.com>
2019-12-20 13:54:43 -0500

    Few changes

---
+++ word_cloud/stylecloud.js
@@ -0,0 +1,47 @@
+
+// Create word cloud
+anychart.onDocumentReady(function() {
+
+     // create a tag (word) cloud chart
+
+    anychart.data.loadCsvFile('stylecloud.csv', function (data) {
+
+      console.log(data);
+
+      var chart = anychart.tagCloud(data);
+
+      // set a chart title
+      chart.title('30 Beer style with most beers');
+
+
+      // set an array of angles at which the words will be laid out
+      chart.angles([0]); 
+      // // configure angles
+      chart.fromAngle(10);
+      chart.toAngle(100);
+      //chart.angles([0,-40,0]);
+
+      chart.hovered().fill("#006600");
+
+      // create and configure a color scale.
+      chart.colorRange(true);
+  // set the color range length
+     chart.colorRange().length('80%');
+
+  
+      // set text spacing
+      chart.textSpacing(0.5);
+
+      var tooltip = chart.tooltip();
+      // enable HTML for tooltips
+      chart.tooltip().useHtml(true);
+      // configure tooltips
+      var formatter = "No. of beers: {%value}"
+     
+       var tooltip = chart.tooltip();
+       tooltip.format(formatter);
+
+      // display the word cloud chart
+      chart.container("container").draw();
+  });
+});
