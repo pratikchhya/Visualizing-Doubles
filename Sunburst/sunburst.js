19d326b661300b94c5bfe0899c17be97ef1982c4
DD-44 <danyel_sp@hotmail.com>
2019-12-22 02:48:12 -0500

    hover info

--- Sunburst/sunburst.js
+++ Sunburst/sunburst.js
@@ -20,29 +20,36 @@ d3.csv("beer_styles.csv").then(function(beers) {
       ids: styles,
       labels: styles,
       parents: parent,
-      // hoverlabel: { bgcolor: "red", font: { color: "white" } },
-      // hovertext: description,
-      fillcolors: beerColors,
+      hovertext: description,
+      meta: ABV,
+      customdata: IBU,
+      hovertemplate:
+        "<b> %{label} </b> <br>" +
+        "<b>Parent</b>: %{parent} <br>" +
+        "<b>ABV</b>: %{meta} <br>" +
+        "<b>IBU</b>: %{customdata}<br>" +
+        "<b>Sytle Info: </b> %{hovertext}",
+      // fillcolors: beerColors,
       // values: [10, 14, 12, 10, 4, 6, 6, 4, 4, 3, 5, 6, 7, 6, 5, 4, 3],
-      // outsidetextfont: { size: 20, color: "#377eb8" },
-      leaf: { opacity: 0.4 },
-      marker: { colors: beerColors, line: { width: 2 } }
+      leaf: { opacity: 0.5 },
+      marker: {
+        colors: beerColors,
+        line: { width: 2 }
+      }
     }
   ];
 
   // layout definition
   var layout = {
     margin: { l: 0, r: 0, b: 0, t: 0 },
-    // sunburstcolorway: beerColors,
-    // extendsunburstcolorway: true,
-    fillcolors: beerColors,
+    paper_bgcolor: "rgba(0,0,0,0)",
     width: 800,
     height: 800
   };
 
-  Plotly.newPlot("myDiv", data, layout, { showSendToCloud: true })
-    .on("mouseenter", mouseEnter)
-    .on("mouseleave", mouseLeave);
+  Plotly.newPlot("myDiv", data, layout, { showSendToCloud: true });
+  // .on("mouseenter", mouseEnter)
+  // .on("mouseleave", mouseLeave);
 
   // let test = document.getElementById("myDiv");
   // let mouseOV = d3.select("myDiv")
@@ -66,22 +73,53 @@ d3.csv("beer_styles.csv").then(function(beers) {
   //         .text(sample)
   //         .property("value", sample);
   // let test = d3.select("myDiv");
-  // var square = svg.append("rect")
-  //   .attr("height", 100)
-  //   .attr("width", 100)
-  //   .attr("x", 10)
-  //   .attr("y", 10)
-  //   .attr("fill", "green")
-  //   .on("mouseenter", mouseEnter)
-  //   .on("mouseleave", mouseLeave);
+  // // var square = svg
+  // //   .append("rect")
+  // //   .attr("height", 100)
+  // //   .attr("width", 100)
+  // //   .attr("x", 10)
+  // //   .attr("y", 10)
+  // //   .attr("fill", "green")
+  // //   .on("mouseenter", mouseEnter)
+  // //   .on("mouseleave", mouseLeave);
 
-  function mouseEnter() {
-    square.attr("fill", "red");
-  }
+  // function mouseEnter() {
+  //   data.fillcolors("red");
+  //   d3.select("Style_Info").append(description);
+  // }
 
-  function mouseLeave() {
-    square.attr("fill", "blue");
-  }
+  // function mouseLeave() {
+  //   data.fillcolors("blue");
+  // }
+
+  var e = document.getElementById("myDiv");
+  e.onmouseover = function() {
+    document.getElementById("popup").style.display = "block";
+  };
+  e.onmouseout = function() {
+    document.getElementById("popup").style.display = "none";
+  };
+
+  // function buildMetadata(styleInfo) {
+  //   var hoverOV = d3.select("myDiv");
+  //   d3.on(MouseEvent).select(this);
+  //     var tbody = d3.select("tbody");
+
+  //     // Use `.html("") to clear any existing metadata
+  //     tbody.html("");
+
+  //     var count = 0;
+  //     var row = tbody.append("tr");
+  //     Object.entries(sample).forEach(([key, value]) => {
+  //       // console.log(key, value);
+  //       if (count < 6) {
+  //         var cell = row.append("td");
+  //         cell.text(value);
+  //         count += 1;
+  //       }
+  //     });
+  //   });
+  // }
 
   // });
 });
