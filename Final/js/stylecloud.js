e338995c2ceddcc782ae28058ac60f77231493ef
saitejav10 <53284323+saitejav10@users.noreply.github.com>
2019-12-30 20:48:44 -0500

    updated word cloud

--- Final/js/stylecloud.js
+++ Final/js/stylecloud.js
@@ -1,43 +1,44 @@
 // Create word cloud
 anychart.onDocumentReady(function() {
 
-    // create a tag (word) cloud chart
+  // create a tag (word) cloud chart
 
-   anychart.data.loadCsvFile('data/stylecloud.csv', function (data) {
+ anychart.data.loadCsvFile('data/stylecloud.csv', function (data) {
 
-     console.log(data);
+   console.log(data);
 
-     var chart = anychart.tagCloud(data);
+   var chart = anychart.tagCloud(data);
+
+   // set a chart title
+   //chart.title('30 Beer style with most beers');
 
 
-     // set an array of angles at which the words will be laid out
-     chart.angles([0]); 
-     // // configure angles
-     chart.fromAngle(10);
-     chart.toAngle(100);
-     //chart.angles([0,-40,0]);
+   // set an array of angles at which the words will be laid out
+   chart.angles([0]); 
+   // // configure angles
+   //chart.fromAngle(10);
+   //chart.toAngle(100);
+   chart.angles([10,-20,-10]);
 
-     chart.hovered().fill("#006600");
+   chart.hovered().fill("#006600");
 
-     // create and configure a color scale.
-     chart.colorRange(true);
- // set the color range length
-    chart.colorRange().length('80%');
+   // create and configure a color scale.
+   //chart.colorRange(true);
 
- 
-     // set text spacing
-     chart.textSpacing(0.5);
 
-     var tooltip = chart.tooltip();
-     // enable HTML for tooltips
-     chart.tooltip().useHtml(true);
-     // configure tooltips
-     var formatter = "No. of beers: {%value}"
-    
-      var tooltip = chart.tooltip();
-      tooltip.format(formatter);
+   // set text spacing
+   chart.textSpacing(0.5);
 
-     // display the word cloud chart
-     chart.container("chart2").draw();
- });
+   var tooltip = chart.tooltip();
+   // enable HTML for tooltips
+   chart.tooltip().useHtml(true);
+   // configure tooltips
+   var formatter = "No. of beers: {%value}"
+  
+    var tooltip = chart.tooltip();
+    tooltip.format(formatter);
+
+   // display the word cloud chart
+   chart.container("chart2").draw();
+});
 });
